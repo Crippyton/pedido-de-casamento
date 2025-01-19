a06301d5853f0bd6c3e3c7b304fbe4d3fa0c57c3
Washington Rocha <86808665+Crippyton@users.noreply.github.com>
2025-01-19 15:42:22 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 const btnNo = document.getElementById('btnNo');
+const btnYes = document.getElementById('btnYes');
+const duckSound = document.getElementById('duckSound');
 
 // Botão "Não" se move ao passar o mouse
 btnNo.addEventListener('mouseover', () => {
@@ -14,8 +16,12 @@ btnNo.addEventListener('click', () => {
 });
 
 // Botão "Sim" redireciona para outro link
-const btnYes = document.getElementById('btnYes');
 btnYes.addEventListener('click', () => {
-  // link de comemoração
-  window.location.href = 'https://youtu.be/D7-r0bxVitk?si=CJtkMoC8QSUevS5h'; 
+  window.location.href = 'https://youtu.be/D7-r0bxVitk?si=CJtkMoC8QSUevS5h';
+});
+
+// Tocar som de pato ao passar o mouse no botão "Sim"
+btnYes.addEventListener('mouseover', () => {
+  duckSound.currentTime = 0; // Reinicia o som, caso já esteja tocando
+  duckSound.play();
 });
